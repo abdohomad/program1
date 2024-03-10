e9f3abdc40050cce922f528e069dd7adf6bcc3f8
Abdalrahman H Bashir <60171770+abdohomad@users.noreply.github.com>
2024-03-10 18:18:43 -0500

    update main

--- src/Graph_manager.py
+++ src/Graph_manager.py
@@ -18,118 +18,101 @@ class GraphManager:
         if not one_way:
             self.graph.add_edge(destination, source, weight=distance)
 
-    def breadth_first_search(self, start, goal):
+    def generic_blind_search(self, start, goal, search_type="bfs"):
+        """
+        Performs a generic search (either BFS or DFS) to find a path from start to goal.
+        """
 
         # Open list (queue) to store nodes to be explored next
         # Initialize with starting node and empty path
         open_list = [(start, [])]
-
-        # Closed list to store explored nodes
-        explored_list = set()
+        explored_list = set()    # Closed list to store explored nodes
 
         while open_list:
-            # Pop the current node and its path from the front of the open list (queue)
-            current, path = open_list.pop(0)
-
+            # Pop the current node and its path from the front of the open list (queue) or
+            # Pop the current node and its path from the open list (stack).
+            current, path = open_list.pop(
+                0) if search_type == "bfs" else open_list.pop()
             # Add the current node to the closed list
             explored_list.add(current)
 
-            # Check if the goal node is reached
             if current == goal:
                 # Path found, return the complete path from start to goal
                 return path + [current], explored_list
-
             # Explore unvisited neighbors
             for neighbor in set(self.graph.neighbors(current)) - explored_list:
                 # Add the neighbor and updated path to the end of the open list
                 open_list.append((neighbor, path + [current]))
 
-        # No path found, return None
         return None, explored_list
+
+    def breadth_first_search(self, start, goal):
+
+        return self.generic_blind_search(start, goal, search_type="bfs")
 
     def depth_first_search(self, start, goal):
 
-        # Open list (stack) to store nodes to be explored next.
-        # Initialize with starting node and empty path.
-        open_list = [(start, [])]
-
-        # Closed list to store explored nodes.
-        explored_list = set()
-
-        while open_list:
-            # Pop the current node and its path from the open list (stack).
-            current, path = open_list.pop()
-
-            # Add the current node to the closed list.
-            explored_list.add(current)
-
-            # Check if the goal node is reached.
-            if current == goal:
-                # Path found, return the complete path from start to goal.
-                return path + [current], explored_list
-
-            # Explore unvisited neighbors
-            for neighbor in set(self.graph.neighbors(current)) - explored_list:
-                # Add the neighbor and updated path to the open list.
-                open_list.append((neighbor, path + [current]))
-
-        # No path found, return None
-        return None, explored_list
+        return self.generic_blind_search(start, goal, search_type="dfs")
 
     def best_first_search(self, start, goal):
 
         open_list = []  # Priority queue for open nodes
-        explored_list = set()  # Set to store explored nodes
+        explored_list = []  # Set to store explored nodes
 
         # Initial node with estimated cost and path
-        heapq.heappush(open_list, (0, start, [start]))
+        if start != goal:
+            heapq.heappush(
+                open_list, (self.heuristics[start, goal], start, [start]))
+        elif start == goal:
+            heapq.heappush(
+                open_list, (0, start, [start]))
 
         while open_list:
             # Get node with lowest estimated cost
             current_cost, current_city, path = heapq.heappop(open_list)
+            explored_list.append(current_city)
+
             # Goal reached, return path
             if current_city == goal:
                 return path + [goal], explored_list
 
             # Explore neighbors
-            for neighbor in set(self.graph.neighbors(current_city)) - explored_list:
-                # Add the neighbor to the explored set
-                explored_list.add(neighbor)
-                if neighbor == goal:
-                    return path + [goal], explored_list
-                next_cost = self.graph[current_city][neighbor]["weight"]
-                heuristic = self.heuristics[neighbor, goal]
-                total_cost = next_cost + heuristic
-                next_node = (total_cost,
-                             neighbor,
-                             path + [neighbor])
-                heapq.heappush(open_list, next_node)
+            for neighbor in set(self.graph.neighbors(current_city)):
+                if neighbor not in explored_list:
+                    if neighbor == goal:
+                        return path + [goal], explored_list
+                    # next_cost = self.graph[current_city][neighbor]["weight"]
+                    heuristic = self.heuristics[neighbor, goal]
+                    total_cost = heuristic
+                    next_node = (total_cost,
+                                 neighbor,
+                                 path + [neighbor])
+                    heapq.heappush(open_list, next_node)
 
         return None, explored_list  # No path found
 
     def a_star_search(self, start_city, goal_city):
 
         open_list = []  # Initialize open list
-        explored_list = set()  # Initialize closed list
+        explored_list = set()  # Initialize explored list
         start_node = (0, start_city, [start_city])
-        heapq.heappush(open_list, start_node)  # Add start node to open list
+        heapq.heappush(open_list, start_node)  # Add start node
         while open_list:
 
             current_cost, current_city, current_path = heapq.heappop(open_list)
-            explored_list.add(current_city)  # Add current node to closed list
-
+            explored_list.add(current_city)
             if current_city == goal_city:
-                # Found the goal, return the path
-                return current_path, explored_list
+                return current_path + [goal_city], explored_list
 
             for neighbor in set(self.graph.neighbors(current_city)) - explored_list:
                 if neighbor == goal_city:
                     return current_path + [goal_city], explored_list
+
                 next_cost = self.graph[current_city][neighbor]["weight"]
                 heuristic = self.heuristics[neighbor, goal_city]
                 total_cost = current_cost + next_cost + heuristic
-                next_node = (total_cost, neighbor, current_path + [neighbor])
-
+                next_node = (total_cost, neighbor,
+                             current_path + [neighbor])
                 # Check for duplicates and update if necessary
                 if neighbor not in explored_list:
                     heapq.heappush(open_list, next_node)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
-import pandas as pd
+from tabulate import tabulate
 from Graph_manager import GraphManager
 from Coordinates import Coordinates
 from geopy.distance import geodesic
-import matplotlib.pyplot as plt
 import time
+import pandas as pd
 
 
 def load_coordinates(file_path):
@@ -13,7 +13,6 @@ def load_coordinates(file_path):
 
 
 def add_cities(graph_manager, city_coordinates):
-
     for city_name in city_coordinates.keys():
         graph_manager.add_city(city_name, city_coordinates.get(city_name))
 
@@ -65,81 +64,6 @@ def calculate_distance(path, city_coordinates):
     return total_distance
 
 
-def main():
-    coordinates_file = "src/coordinates.csv"
-    adjacency_file = "src/Adjacencies.txt"
-
-    # Load coordinates from the CSV file
-    city_coordinates = load_coordinates(coordinates_file)
-
-    # Create an instance of the GraphManager
-    graph_manager = GraphManager()
-
-    add_cities(graph_manager, city_coordinates)
-    # Add cities to the graph and establish connections
-    add_connections(graph_manager, city_coordinates, adjacency_file)
-
-    start_city = input("Enter the starting city: ")
-    goal_city = input("Enter the destination city: ")
-
-    start_coords, goal_coords = city_coordinates.get(
-        start_city), city_coordinates.get(goal_city)
-
-    if not start_coords or not goal_coords:
-        print(f"Coordinates not found for {start_city} or {goal_city}")
-        return
-
-    graph_manager.compute_heuristics()
-
-    # Define search algorithms
-    search_algorithms = {
-        "1": "Breadth-First Search",
-        "2": "Depth-First Search",
-        "3": "Best-First Search",
-        "4": "A* Search"
-    }
-
-    function1_choice = input("Enter the first function choice (1-4): ")
-    function2_choice = input("Enter the second function choice (1-4): ")
-
-    if function1_choice not in search_algorithms or function2_choice not in search_algorithms:
-        print("Invalid function choices. Please enter numbers between 1 and 4.")
-        return
-
-    function1_algorithm = search_algorithms[function1_choice]
-    function2_algorithm = search_algorithms[function2_choice]
-
-    start_time = time.time()  # Start time measurement
-
-    solution_path1, visited1 = execute_search_function(
-        graph_manager, function1_choice, start_city, goal_city)
-
-    elapsed_time1 = time.time() - start_time
-
-    start_time = time.time()  # Reset start time for the second function
-
-    solution_path2, visited2 = execute_search_function(
-        graph_manager, function2_choice, start_city, goal_city)
-
-    elapsed_time2 = time.time() - start_time
-
-    # Display results as a table
-    results_table = pd.DataFrame({
-        "Algorithm": [function1_algorithm, function2_algorithm],
-        "Visited Cities": [len(visited1) if visited1 else None, len(visited2) if visited2 else None],
-        "Total Distance": [calculate_distance(solution_path1, city_coordinates) if solution_path1 else None,
-                           calculate_distance(solution_path2, city_coordinates) if solution_path2 else None],
-        "Search Time": ["{:.9f}".format(elapsed_time1), "{:.9f}".format(elapsed_time2)]
-    })
-
-    print("\nSearch Results:")
-    print(results_table)
-    print('////////////////')
-    print(visited1)
-    print("/////")
-    print(visited2)
-
-
 def execute_search_function(graph_manager, choice, start_city, goal_city):
     if choice == "1":
         return graph_manager.breadth_first_search(start_city, goal_city)
@@ -151,6 +75,122 @@ def execute_search_function(graph_manager, choice, start_city, goal_city):
         return graph_manager.a_star_search(start_city, goal_city)
     else:
         return None, None
+
+
+def get_user_input(prompt, validation_func=None):
+    while True:
+        user_input = input(prompt)
+        if validation_func and not validation_func(user_input):
+            print("Invalid input. Please try again.")
+        else:
+            return user_input
+
+
+def validate_city_choice(choice, available_cities):
+    return choice in available_cities
+
+
+def validate_algorithm_choice(choice):
+    return choice in {"1", "2", "3", "4"}
+
+
+def display_results(results_table, solution_path1, solution_path2):
+    print("\nResults Table:")
+    print(tabulate(results_table, headers='keys', tablefmt='pretty'))
+    print("\nPath for Function 1:")
+    print(solution_path1)
+    print("\nPath for Function 2:")
+    print(solution_path2)
+
+
+def main():
+    while True:
+        coordinates_file = "src/coordinates.csv"
+        adjacency_file = "src/Adjacencies.txt"
+
+        # Load coordinates from the CSV file
+        city_coordinates = load_coordinates(coordinates_file)
+
+        # Create an instance of the GraphManager
+        graph_manager = GraphManager()
+
+        add_cities(graph_manager, city_coordinates)
+        # Add cities to the graph and establish connections
+        add_connections(graph_manager, city_coordinates, adjacency_file)
+
+        available_cities = list(city_coordinates.keys())
+
+        # Get the starting city with input validation
+        start_city = get_user_input(
+            "Enter the starting city: ", lambda choice: validate_city_choice(choice, available_cities))
+
+        # Get the destination city with input validation
+        goal_city = get_user_input(
+            "Enter the destination city: ", lambda choice: validate_city_choice(choice, available_cities))
+
+        start_coords, goal_coords = city_coordinates.get(
+            start_city), city_coordinates.get(goal_city)
+
+        if not start_coords or not goal_coords:
+            print(f"Coordinates not found for {start_city} or {goal_city}")
+            return
+
+        graph_manager.compute_heuristics()
+
+        # Define search algorithms
+        search_algorithms = {
+            "1": "Breadth-First Search",
+            "2": "Depth-First Search",
+            "3": "Best-First Search",
+            "4": "A* Search"
+        }
+
+        function1_choice = get_user_input(
+            "Enter the first function choice (1-4): ", lambda choice: validate_algorithm_choice(choice))
+        function2_choice = get_user_input(
+            "Enter the second function choice (1-4): ", lambda choice: validate_algorithm_choice(choice))
+
+        if function1_choice not in search_algorithms or function2_choice not in search_algorithms:
+            print("Invalid function choices. Please enter numbers between 1 and 4.")
+            return
+
+        function1_algorithm = search_algorithms[function1_choice]
+        function2_algorithm = search_algorithms[function2_choice]
+
+        start_time = time.time()  # Start time measurement
+
+        solution_path1, visited1 = execute_search_function(
+            graph_manager, function1_choice, start_city, goal_city)
+
+        elapsed_time1 = time.time() - start_time
+
+        start_time = time.time()  # Reset start time for the second function
+
+        solution_path2, visited2 = execute_search_function(
+            graph_manager, function2_choice, start_city, goal_city)
+
+        elapsed_time2 = time.time() - start_time
+
+        # Display results as a table
+        results_table = pd.DataFrame({
+            "Algorithm": [function1_algorithm, function2_algorithm],
+            "Visited Cities": [len(visited1) if visited1 else None, len(visited2) if visited2 else None],
+            "Total Distance": [calculate_distance(solution_path1, city_coordinates) if solution_path1 else None,
+                               calculate_distance(solution_path2, city_coordinates) if solution_path2 else None],
+            "Search Time": ["{:.9f}".format(elapsed_time1), "{:.9f}".format(elapsed_time2)]
+        })
+
+        display_results(results_table, solution_path1, solution_path2)
+
+        # Ask the user if they want to perform another comparison
+        another_comparison = input(
+            "\nDo you want to perform another comparison? (yes/no): ").lower()
+        if another_comparison != 'yes':
+            break
+
+
+if __name__ == "__main__":
+    main()
 
 
 if __name__ == "__main__":
